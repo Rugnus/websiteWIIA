6ac392288486751aa86966b9f110ebbd181a4941
Gasanov Sungur <sungur.gasanov@mail.ru>
2020-11-24 15:52:29 +0300

    Project has done!

---
+++ index.js
@@ -0,0 +1,121 @@
+$('#normal').click(function() {
+    var snt = window.speechSynthesis,
+    mess = new SpeechSynthesisUtterance('Normal page');
+    mess.lang = 'en-US';
+    snt.speak(mess);
+});
+
+$('#blindpage').click(function() {
+    var snt = window.speechSynthesis,
+    mess = new SpeechSynthesisUtterance('Page for blind people');
+    mess.lang = 'en-US';
+    snt.speak(mess);
+});
+
+$('#items').click(function() {
+    var snt = window.speechSynthesis,
+    mess = new SpeechSynthesisUtterance('Preferences');
+    mess.lang = 'en-US';
+    snt.speak(mess);
+});
+
+$('#mainHeader').click(function() {
+    var snt = window.speechSynthesis,
+    mess = new SpeechSynthesisUtterance('Menu');
+    mess.lang = 'en-US';
+    snt.speak(mess);
+});
+
+$('#aboutus').click(function() {
+    var snt = window.speechSynthesis,
+    mess = new SpeechSynthesisUtterance('About us');
+    mess.lang = 'en-US';
+    snt.speak(mess);
+});
+
+$('#aboutUsText').click(function() {
+    var snt = window.speechSynthesis;
+    // mess = new SpeechSynthesisUtterance('Lorem ipsum dolor sit amet consectetur adip totamilique quibusdam rem tenetur sed voluptatibus eveniettotamilique quibusdam rem tenetur sed voluptatibus eveniettotamilique quibusdam rem tenetur sed voluptatibus eveniettotamilique quibusdam rem tenetur sed voluptatibus eveniet');
+    mess.lang = 'en-US';
+    snt.speak(mess);
+});
+
+$('#prefTitle').click(function() {
+    var snt = window.speechSynthesis,
+    mess = new SpeechSynthesisUtterance('Our preferences');
+    mess.lang = 'en-US';
+    snt.speak(mess);
+});
+
+
+//???????????
+$('#item1').click(function() {
+    var snt = window.speechSynthesis,
+    mess = new SpeechSynthesisUtterance('Disability');
+    mess.lang = 'en-US';
+    snt.speak(mess);
+});
+
+$('#item2').click(function() {
+    var snt = window.speechSynthesis,
+    mess = new SpeechSynthesisUtterance('Blind');
+    mess.lang = 'en-US';
+    snt.speak(mess);
+});
+
+$('#item3').click(function() {
+    var snt = window.speechSynthesis,
+    mess = new SpeechSynthesisUtterance('Deaf');
+    mess.lang = 'en-US';
+    snt.speak(mess);
+});
+
+$('#whatwedo').click(function() {
+    var snt = window.speechSynthesis,
+    mess = new SpeechSynthesisUtterance('What we do');
+    mess.lang = 'en-US';
+    snt.speak(mess);
+});
+
+$('#helpsdis').click(function() {
+    var snt = window.speechSynthesis,
+    mess = new SpeechSynthesisUtterance('Helps disability people');
+    mess.lang = 'en-US';
+    snt.speak(mess);
+});
+
+$('#helpsblind').click(function() {
+    var snt = window.speechSynthesis,
+    mess = new SpeechSynthesisUtterance('Helps blindless people');
+    mess.lang = 'en-US';
+    snt.speak(mess);
+});
+
+$('#foot').click(function() {
+    var snt = window.speechSynthesis,
+    mess = new SpeechSynthesisUtterance('Contacts');
+    mess.lang = 'en-US';
+    snt.speak(mess);
+});
+
+$('#facebook').click(function() {
+    var snt = window.speechSynthesis,
+    mess = new SpeechSynthesisUtterance('Facebook');
+    mess.lang = 'en-US';
+    snt.speak(mess);
+});
+
+$('#vk').click(function() {
+    var snt = window.speechSynthesis,
+    mess = new SpeechSynthesisUtterance('Vk');
+    mess.lang = 'en-US';
+    snt.speak(mess);
+});
+
+$('#inst').click(function() {
+    var snt = window.speechSynthesis,
+    mess = new SpeechSynthesisUtterance('Instagram');
+    mess.lang = 'en-US';
+    snt.speak(mess);
+});
+
